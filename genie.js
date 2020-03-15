253e14a11ce2697b7237767d2fd924a46a538242
Micah Cowan <micah@addictivecode.org>
2020-03-15 04:44:35 -0700

    Fixed, working.

--- genie.js
+++ genie.js
@@ -6,81 +6,47 @@ Input.prototype = new (function() {
         const _self = this;
         const elem = this.elem;
         elem.addEventListener('input', () => cb(parseInt(elem.value, 16)));
+        cb(parseInt(elem.value, 16));
     };
 });
 
 function Genie(addrF, dataF, cmpF, out) {
-    this.addr = this.register(new Input(addrF), "1dEfBcDe");
-    this.data = this.register(new Input(dataF), "AbHa");
-    this.cmp  = this.register(new Input(cmpF), "GhFg");
     this.out = out;
+    this.addr = this.data = this.cmp = 0;
     this.value = 0x00800000; // For the hard-coded "on" high bit of addr
-
-    this.sanityCheckRegistry();
+    this.register(new Input(addrF), 'addr');
+    this.register(new Input(dataF), 'data');
+    this.register(new Input(cmpF), 'cmp');
 }
 Genie.prototype = new (function() {
     this.register = function(input, mapping){
         const _self = this;
-        this.registerMapping(mapping);
         input.listen((value) => _self.recv(value, mapping));
         return input;
     };
-    this.registerMapping = function(mapping){
-        let reg = this.maybeInitMappingRegistry();
-        let i = 0;
-        for (let item of mapping) {
-            if (!(item in reg)) {
-                throw `Unrecognized mapping ${item}`;
-            }
-            if (item != '1'
-                    && ( (item == item.toUpperCase()) != ((i % 2) == 0) )) {
-                throw `'${item}' of '${mapping}' is out of place!`;
-            }
-            ++reg[item];
-            ++i;
-        }
-    };
-    const mapstr = "abcdefgh";
-    this.maybeInitMappingRegistry = function(){
-        if (this.mappingReg === undefined) {
-            this.mappingReg = {};
-            for (let c_ of mapstr) {
-                for (let c of [c_.toUpperCase(), c_.toLowerCase()]) {
-                    if (c == 'C') c = '1';
-                    this.mappingReg[c] = 0;
-                }
-            }
-        }
-        return this.mappingReg;
-    };
-    this.sanityCheckRegistry = function(){
-        for (m in this.mappingReg) {
-            if (this.mappingReg[m] == 0) {
-                throw `No mappings for ${m}!`;
-            }
-            else if (this.mappingReg[m] > 1) {
-                throw `Multiple mappings (${this.mappingReg[m]}) for ${m}!`;
-            }
-        }
-    };
-    this.recv = function(value, mapping){
-        this.adjustValue(value, mapping);
+    this.recv = function(value, name) {
+        this[name] = value;
         this.output();
     }
-    this.adjustValue = function(value, mapping) {
-        let i=0;
-        for (m of mapping) {
+    let mapping = "1dEfBcDeAbHaGhFg";
+    this.recalc = function() {
+        let inval = ((this.addr & 0xFFFF) << 16) | ((this.data & 0xFF) << 8) | (this.cmp & 0xFF);
+        let outval = 0x00800000;
+        for (let i=0; i != mapping.length; ++i) {
+           m = mapping[i];
            if (m == '1') continue;
            let baseMask = (m == m.toUpperCase())? 8: 7;
-           let srcWhere = 4 * (Math.floor(mapping.length / 2) - 1 - (i - (i%2)));
+           let srcWhere = 4 * (Math.floor((mapping.length+1 - i) / 2) - 1);
            let dstWhere = 28 - (m.toUpperCase().charCodeAt(0) - 0x41 /* A */) * 4;
 
-           this.value = (this.value & ~(baseMask << dstWhere))
-                | (((value & (baseMask << srcWhere)) >> srcWhere) << dstWhere);
-           ++i;
+           let inMask = baseMask << srcWhere;
+           let inGot  = (inval & inMask) >>> srcWhere;
+           outval |= inGot << dstWhere;
         }
+        this.value = outval;
     };
     this.output = function() {
+        this.recalc();
         let t = this.transliterate(this.value);
         this.out.innerText = t;
     }
@@ -88,7 +54,7 @@ Genie.prototype = new (function() {
     this.transliterate = function(value) {
         let ret = "";
         for (let i=28; i>=0; i-=4) {
-            ret += transLookup[ (this.value >> i) & 0xF ];
+            ret += transLookup[ (value >> i) & 0xF ];
         }
 
         return ret;
